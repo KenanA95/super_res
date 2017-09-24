66b9bfa02220692712118601114c4eb705a28d2d
Kenan <kenan.r.alkiek@gmail.com>
2017-09-24 13:35:52 -0400

    Align low-res frames based on the star centroids

--- src/registration.py
+++ src/registration.py
@@ -1,8 +1,27 @@
+import numpy as np
 from scipy import ndimage
+from scipy.ndimage import fourier_shift
 from skimage.filters import threshold_otsu
+from matplotlib import pyplot as plt
 
 
 def calculate_centroid(im):
     thresh = threshold_otsu(im)
     binary = im > thresh
-    return ndimage.measurements.center_of_mass(binary)[::-1]
+    return ndimage.measurements.center_of_mass(binary)
+
+
+# Align the stars according to their centroids
+def centroid_align(stars):
+
+    center = int(stars[0].shape[0] / 2), int(stars[0].shape[1] / 2)
+    aligned = []
+    for star in stars:
+        centroid = calculate_centroid(star)
+
+        shift = np.subtract(center, centroid)
+        star = fourier_shift(np.fft.fftn(star), shift)
+        star = np.fft.ifftn(star).real
+        aligned.append(star)
+
+    return aligned
