c15baaac7b077685d95da4f46795bfa6d7485946
Kenan <kenan.r.alkiek@gmail.com>
2017-10-09 12:15:08 -0400

    Corrections and docstring for decimation matrix

--- src/linear_operator.py
+++ src/linear_operator.py
@@ -21,21 +21,37 @@ def transform_coordinates(x, y, tf):
     return int(X), int(Y)
 
 
-# Matrix to decimate a single high-res image by a given factor
 def decimation_matrix(target_resolution, downsample_factor):
+    """
+        Matrix operator to decimate an image by a given factor
+        Reference: http://users.wfu.edu/plemmons/papers/siam_maa3.pdf ( sect. A.1)
 
+        Parameters
+        ----------
+        target_resolution : int
+            Size of the high-resolution image
+        downsample_factor : int
+            How much to decimate the image by
+
+        Returns
+        -------
+        Sparse matrix of size (lr^2 x hr^2) that down-samples the image through multiplication
+
+    """
     lr_size = int(target_resolution / downsample_factor)
 
     # Get a grid the size of the lr frame to represent the indices
-    row_indices, col_indices = np.meshgrid(range(lr_size), range(lr_size))
+    rows, cols = np.meshgrid(range(lr_size), range(lr_size))
 
     # Change the sampling rate ex. (indices = 0, 1, 2..., factor = 2) => 0, 2, 4...
-    row_indices = row_indices * downsample_factor
-    col_indices = col_indices * downsample_factor
+    sampled_rows = rows * downsample_factor
+    sampled_cols = cols * downsample_factor
 
     # Convert the indices to the decimation matrix
-    sparse_col_indices = np.ravel_multi_index((row_indices, col_indices), dims=(target_resolution, target_resolution)).T
-    sparse_row_indices = np.ravel_multi_index((lr_size, lr_size), dims=(lr_size, lr_size)).T
+    sparse_col_indices = np.ravel_multi_index((sampled_rows, sampled_cols),
+                                              dims=(target_resolution, target_resolution)).T
+
+    sparse_row_indices = np.ravel_multi_index((rows, cols), dims=(lr_size, lr_size)).T
 
     data = np.ones(len(sparse_row_indices) * lr_size)
     shape = (lr_size ** 2, target_resolution ** 2)
