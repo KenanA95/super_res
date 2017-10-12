cbe0ea404e6a2b92c02090e8a2a3cd276f815a66
Kenan <kenan.r.alkiek@gmail.com>
2017-10-12 12:48:03 -0400

    Test blur operator

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -1,6 +1,9 @@
 import unittest
-from linear_operator import decimation_matrix
+from linear_operator import decimation_matrix, blur_matrix
 from skimage import data
+import numpy as np
+from scipy.signal import convolve2d
+import numpy.testing as npt
 
 
 class TestOperator(unittest.TestCase):
@@ -16,12 +19,27 @@ class TestOperator(unittest.TestCase):
         im = data.camera()
 
         # Decimate the image by a factor of 4
-        dec_mat = decimation_matrix(im.shape[0], im.shape[1], downsample_factor=4)
-        im = dec_mat * im.flat
+        dec_operator = decimation_matrix(im.shape[0], im.shape[1], downsample_factor=4)
+        im = dec_operator * im.flat
 
         # Result should be 128x128 represented as a vector
         self.assertEqual(len(im), 128**2)
 
+    def test_blur(self):
+
+        # 200x200 image and spatially-invariant psf
+        im = data.checkerboard()
+        psf = np.ones((5, 5)) / 5**2
+
+        # Use the operator to convolve
+        blur_operator = blur_matrix(im.shape[0], im.shape[1], psf)
+        im_blurred = blur_operator * im.flat
+        im_blurred = np.reshape(im_blurred, (200, 200))
+
+        # Get the actual zero boundary convolution
+        actual = convolve2d(im, psf, 'same', boundary='fill', fillvalue=0)
+
+        npt.assert_equal(np.rint(im_blurred), np.rint(actual))
 
 if __name__ == '__main__':
     unittest.main()
