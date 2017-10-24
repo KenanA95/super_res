9e8bbb1334ac67cea54a55c59101ec005f62c913
Kenan <kenan.r.alkiek@gmail.com>
2017-10-24 10:40:50 -0400

    Changed comment format to match rest of project

--- orex/extract_stars.py
+++ orex/extract_stars.py
@@ -6,13 +6,7 @@ from scipy.ndimage.filters import gaussian_filter
 
 
 def get_grid(im, center, size):
-    """Extract a sub-matrix from the image array around a given point
-
-    :param im: The image to extract the sub-matrix from
-    :param center: The center of the grid to be extracted
-    :param size: The diameter of the grid. Ex. size=5 -> grid=5x5
-    :return: A sub-matrix from the image around the center coordinates provided
-    """
+    """Extract a sub-matrix from the image array around a given point """
     offset = int((size - 1) / 2)
 
     return im[center[0] - offset:center[0] + offset + 1,
@@ -20,14 +14,7 @@ def get_grid(im, center, size):
 
 
 def set_grid(im, center, size, value=0):
-    """ Set a sub-matrix within an image to a given value
-
-    :param im: The image the sub-matrix is in
-    :param center: The center of the grid to be set
-    :param size: The diameter of the grid. Ex. size=5 -> grid=5x5
-    :param value: The value to set the sub-matrix to
-    :return: The altered image
-    """
+    """ Set a sub-matrix within an image to a given value """
     offset = int((size - 1) / 2)
 
     im[center[0] - offset:center[0] + offset + 1,
@@ -37,13 +24,25 @@ def set_grid(im, center, size, value=0):
 
 
 def star_locations(im, n, sigma, saturation_thresh):
-    """ Find the locations of stars in the NavCam images by blurring the image and selecting the brightest locations
+    """
+        Find the locations of stars in the NavCam images by blurring the image and selecting the brightest locations
 
-    :param im: The image to search in
-    :param n: The number of locations to fine
-    :param sigma: The sigma value for the gaussian filter. Controls the level of blur
-    :param saturation_thresh: DN value indicating a pixel is over saturated and the star data is not reliable
-    :return: x, y pixel coordinates of the stars stored in a list
+        Parameters
+        ----------
+        im: ndarray
+            Image to search in
+        n: int
+            The number of locations to find
+        sigma
+            Sigma value for the gaussian filter. Controls the level of blur when searching for stars
+        saturation_thresh
+            DN value indicating a pixel is over saturated and the star should be ignored
+
+        Returns
+        -------
+        locations: list
+            List of (x, y) pixel coordinates
+
     """
     height, width = im.shape[:2]
     blurred = gaussian_filter(im, sigma=sigma)
@@ -71,11 +70,22 @@ def star_locations(im, n, sigma, saturation_thresh):
 
 def extract_stars(im, locations, size):
     """
+        Extract the stars from a NavCam image by locating the brightest points
 
-    :param im: The image to grab the stars from
-    :param locations: List of the stars pixel coordinates
-    :param size: The diameter of the grid. Ex. size=5 -> grid=5x5
-    :return: A list of the stars represented as matrices
+        Parameters
+        ----------
+        im: ndarray
+            The image to grab the stars from
+        locations: list
+            List of the stars pixel coordinates
+        size: int
+            The diameter of the grid. Ex. size=5 -> grid=5x5
+
+        Returns
+        -------
+        stars: list
+            A list of the stars represented as matrices
+
     """
     stars = []
     for loc in locations:
@@ -87,8 +97,10 @@ def extract_stars(im, locations, size):
     return stars
 
 
-# Split an image into a list of blocks
 def divide_image(image, shape):
+    """
+        Split an image into a list of blocks
+    """
     x, y = shape
     vertical_sections = np.vsplit(image, x)
     blocks = np.empty((x * y), dtype=object)
