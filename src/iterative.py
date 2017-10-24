f079a3d2065dededa68bd646b89af60c4b3e6bb7
Kenan <kenan.r.alkiek@gmail.com>
2017-10-23 22:25:57 -0400

    Removed unnessecary argument

--- src/iterative.py
+++ src/iterative.py
@@ -17,7 +17,7 @@ def stack_low_res(low_res):
     return b
 
 
-def gradient_descent(low_res, A, x0, upsample_factor, iterations, damp=1e-1):
+def gradient_descent(low_res, A, x0, iterations, damp=1e-1):
     """
         Solve Ax=b through steepest descent optimization
         Reference: http://scholar.sun.ac.za/handle/10019.1/5189 ( sect. 7.4.1)
@@ -30,8 +30,6 @@ def gradient_descent(low_res, A, x0, upsample_factor, iterations, damp=1e-1):
             Sparse operator representing Decimation + Blur
         x0: ndarray
             The initial guess for the high-resolution image. Typically the upsampled average image
-        upsample_factor: int
-            How much to scale the resolution
         iterations: int
             Number of iterations performed for the steepest descent
         damp: float
@@ -47,7 +45,7 @@ def gradient_descent(low_res, A, x0, upsample_factor, iterations, damp=1e-1):
     b = stack_low_res(low_res)
 
     # Get the dimensions of the new high-resolution image
-    M, N = low_res[0].shape[0] * upsample_factor, low_res[0].shape[1] * upsample_factor
+    M, N = x0.shape[0], x0.shape[1]
 
     x = x0.copy().flat
 
@@ -60,7 +58,7 @@ def gradient_descent(low_res, A, x0, upsample_factor, iterations, damp=1e-1):
     return np.reshape(x, (M, N))
 
 
-def lsqr_restore(low_res, A, x0, upsample_factor, iter_lim, atol=1e-8, btol=1e-8, damp=1e-1):
+def lsqr_restore(low_res, A, x0, iter_lim, atol=1e-8, btol=1e-8, damp=1e-1):
     """
             Solve Ax=b through least-solutions solution
             Reference: http://scholar.sun.ac.za/handle/10019.1/5189 ( sect. 7.4.1)
@@ -73,8 +71,6 @@ def lsqr_restore(low_res, A, x0, upsample_factor, iter_lim, atol=1e-8, btol=1e-8
                 Sparse operator representing Decimation + Blur
             x0: ndarray
                 The initial guess for the high-resolution image. Typically the upsampled average image
-            upsample_factor: int
-                How much to scale the resolution
             iter_lim: int
                Explicit limitation on number of iterations (for safety)
             damp: float
@@ -89,7 +85,7 @@ def lsqr_restore(low_res, A, x0, upsample_factor, iter_lim, atol=1e-8, btol=1e-8
     b = stack_low_res(low_res)
 
     # Get the dimensions of the new high-resolution image
-    M, N = low_res[0].shape[0] * upsample_factor, low_res[0].shape[1] * upsample_factor
+    M, N = x0.shape[0], x0.shape[1]
 
     x0 = x0.flat
     b = b - A*x0
